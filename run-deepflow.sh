1a81b07f909d6ae4782c6d8969f1f9781355e284
Marcel Schwittlick <marzzzel@gmail.com>
2017-12-11 08:08:52 +0100

    replaces deepflow with dis of

--- run-deepflow.sh
+++ run-deepflow.sh
@@ -2,9 +2,9 @@ if [ "$#" -ne 3 ]; then
   echo "This is an auxiliary script for makeOptFlow.sh. No need to call this script directly."
   exit 1
 fi
-if [ ! -f deepmatching-static ] && [ ! -f deepflow2-static ]; then
+if [ ! -f deepmatching-static ] && [ ! -f run_OF_RGB ]; then
   echo "Place deepflow2-static and deepmatching-static in this directory."
   exit 1
 fi
 
-./deepmatching-static $1 $2 -nt 10 | ./deepflow2-static $1 $2 $3 -match
+./run_OF_RGB $1 $2 $3
